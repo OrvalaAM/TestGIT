4e63de134c294023e31fc1d58ff942a67fc552d4
OrvalaAM <orvalavala@gmail.com>
2023-03-20 00:15:12 +0700

    menambahkan class SimpleDataBase

--- src/Paket/NewMain.java
+++ src/Paket/NewMain.java
@@ -1,11 +1,39 @@
 package Paket;
 
+import java.time.LocalDate;
+//import java.util.Date;
+import java.util.ArrayList;
 
+class SimpleDataBase <T>{
+    private ArrayList<T> storedData;
+    private ArrayList<LocalDate> inputDate;
+
+    public SimpleDataBase() {
+        this.storedData = new ArrayList<>();
+        this.inputDate = new ArrayList<>();
+    }
+    
+    public void addNewData(T data){
+        this.storedData.add(data);
+        this.inputDate.add(LocalDate.now());
+    }
+    
+    public void printAllData(){
+        for(int i = 0; i < storedData.size(); i++){
+            System.out.println(storedData.get(i) + " " +inputDate.get(i));
+        }
+    }
+}
 
 public class NewMain {
 
     public static void main(String[] args) {
-        
+        SimpleDataBase db = new SimpleDataBase();
+        db.addNewData("Orvala");
+        db.addNewData(12);
+        db.addNewData(2.5);
+        db.addNewData('f');
+        db.printAllData();
     }
     
 }
