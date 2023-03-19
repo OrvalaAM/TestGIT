605413ef38d3fc93fca0461b9f6939411d9d7e23
OrvalaAM <orvalavala@gmail.com>
2023-03-20 00:00:23 +0700

    menambahkan class Penjumlahan dengan method JumlahTigaAngka

--- src/Paket/NewMain.java
+++ src/Paket/NewMain.java
@@ -1,11 +1,15 @@
 package Paket;
 
-
+class Penjumlahan{
+    static void jumlahTigaAngka(int a, int b, int c){
+        System.out.println(a + b + c);
+    }
+}
 
 public class NewMain {
 
     public static void main(String[] args) {
-        
+        Penjumlahan.jumlahTigaAngka(12, 12, 12);
     }
     
 }
